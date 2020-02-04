97c12e37fc0b71de462c8033d9e3b90001949671
flacarrara <flacarrara@gmail.com>
2020-02-03 22:16:16 -0300

    new strcmp_bonus (revised)

--- ft_strcmp_bonus.c
+++ ft_strcmp_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fde-capu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 14:22:20 by fde-capu          #+#    #+#             */
-/*   Updated: 2020/02/03 14:22:38 by fde-capu         ###   ########.fr       */
+/* U20200203215234 :|::||                      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	return (ft_strncmp(s1, s2, \
-		ft_strlen(s1) > ft_strlen(s2) ? ft_strlen(s1) : ft_strlen(s2)));
+	int x;
+	
+	x = ft_strlen(s1) > ft_strlen(s2) ? \
+		(int)ft_strlen(s1) : (int)ft_strlen(s2);
+	return (ft_strncmp(s1, s2, x));
 }
