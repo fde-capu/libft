01c941583dab90d5382568364cbbd116628d356c
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-06-15 08:13:47 -0300

    mrt libft

--- keys_fun_1.c
+++ keys_fun_1.c
@@ -1,42 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   keys.c                                             :+:      :+:    :+:   */
+/*   keys_fun_1.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/10 19:07:30 by fde-capu          #+#    #+#             */
-/*   Updated: 2020/06/12 00:02:30 by fde-capu         ###   ########.fr       */
+/*   Created: 2020/06/11 23:23:53 by fde-capu          #+#    #+#             */
+/*   Updated: 2020/06/12 00:05:14 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "keys.h"
 
-void	key_init(int k)
+int		on_press(int kc, int (fun)())
 {
-	if (k < KEYS_LAST)
-	{
-		key(k)->k_press = on_press;
-		key(k)->k_release = on_release;
-		key_init(k + 1);
-	}
-	return ;
-}
-
-int		keys_init(void)
-{
-	g_key = ft_calloc(sizeof(t_key) * (KEYS_LAST - KEYS_FIRST), 1);
-	key_init(KEYS_FIRST);
+	key(kc)->st = KEY_ON;
+	DEBINT2("pressed", kc, key(kc)->st);
+	fun();
 	return (1);
 }
 
-int		keys_destroy(void)
+int		on_release(int kc, int (fun)())
 {
-	free(g_key);
+	key(kc)->st = KEY_OFF;
+	DEBINT2("release", kc, key(kc)->st);
+	fun();
 	return (1);
-}
-
-t_key	*key(int id)
-{
-	return (&g_key[id]);
 }
